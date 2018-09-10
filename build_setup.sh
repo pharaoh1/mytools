2ac9ff0a0729bf64e6a7cd939a243aaa8f767dd9
RebelLion420 <gaigecarlos@gmail.com>
2018-09-10 23:56:54 +0000

    build.sh: further optimized
    Signed-off-by: RebelLion420 <gaigecarlos@gmail.com>

--- build_setup.sh
+++ build_setup.sh
@@ -1,19 +1,20 @@
+sudo add-apt-repository ppa:keithw/mosh
 sudo apt-get update
 sudo apt full-upgrade -y
-sudo apt-get install build-essential bc libncurses5-dev libelf-dev python-all-dev diffutils colordiff vim rsync tmux -y
+sudo apt-get install mosh build-essential bc libncurses5-dev libelf-dev python-all-dev python-software-properties diffutils colordiff vim rsync tmux -y
+# Import .tmux.conf
+cat tmux.conf > ~/.tmux.conf
 # Install ccache
 sudo apt-get install ccache -y
 sudo /usr/sbin/update-ccache-symlinks
 echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.bashrc
 source ~/.bashrc && echo $PATH
 # Install gdrive
-wget "https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download"
-mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
-chmod +x gdrive
 sudo install gdrive /usr/local/bin/gdrive
 gdrive list
-sleep 5
-dpkg-reconfigure tzdata
+sudo dpkg-reconfigure tzdata
 # Clone kernel source
+cd ~
 git clone -b dev https://github.com/RebelLion420/kernel-msm_perry ok
 cd ok
+tmux a
