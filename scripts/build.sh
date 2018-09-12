a96d90dd1e74d9de4ae8358fa20d6419ea5fa960
RebelLion420 <gaigecarlos@gmail.com>
2018-09-12 14:57:07 -0700

    scripts: Added more generic scripts
    personal: Moved personal files here
    
    Signed-off-by: RebelLion420 <gaigecarlos@gmail.com>

---
+++ scripts/build.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+if [ -z $2 ] || [ -z $3 ]
+then
+	printf "\nUsage: \n\n\tbash build.sh [thread_amount] version_# release_type\n\n\tNOTE: '[thread_amount]' can be an integer or 'auto'.\n\n"
+	exit 1
+fi
+
+KDIR=$PWD
+AK2DIR=~/AnyKernel2
+TCDIR=~/mytools/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+DATE=$(date +"%m%d%y")
+KNAME="OrgasmKernel"
+
+export ARCH=arm && export SUBARCH=arm
+export CROSS_COMPILE=$TCDIR
+export USE_CCACHE=1
+export COMPRESS_CACHE=1
+export DEVICE="perry"
+export KBUILD_BUILD_USER="RblLn"
+export KBUILD_BUILD_HOST="PleasureBox"
+VER="-v$2"
+TYPE="_$3"
+export FINAL_ZIP="$KNAME"-"$DEVICE""$TYPE""$VER"_"$DATE".zip
+
+if [ -e  out/arch/arm/boot/zImage ];
+	then
+	rm -r out #Just to make sure it doesn't make flashable zip with previous zImage
+	make clean
+	make mrproper
+	mkdir out
+fi;
+
+if [ "$1" == 'auto' ]
+then
+	t=$(nproc --all)
+else
+	t=$1
+fi
+GCCV=$("$CROSS_COMPILE"gcc -v 2>&1 | tail -1 | cut -d ' ' -f 3)
+printf "\nTHREADS: $t\nVERSION: $2\nRELEASE: $3\nGCC VERSION: $GCCV\n\n"
+echo "==> Adapted build script, courtest of @facuarmo"
+echo "==> Making kernel binary..."
+make O=out perry_defconfig
+make O=out -j$t zImage
+if [ $? -ne 0 ]
+then
+	echo "!!! Kernel compilation failed, can't continue !!!"
+	exit 1
+fi
+echo "=> Making modules..."
+make O=out -j$t modules
+if [ $? -ne 0 ]
+then
+	echo "Module compilation failed, can't continue."
+	exit 1
+fi
+rm -rf out/modinstall
+mkdir out/modinstall
+make O=out -j$t modules_install INSTALL_MOD_PATH=modinstall INSTALL_MOD_STRIP=1
+if [ $? -ne 0 ]
+then
+	echo "Module installation failed, can't continue."
+	exit 1
+fi
+
+echo "==> Kernel compilation completed"
+
+echo "==> Making Flashable zip"
+
+echo "=> Finding modules"
+
+rsync -aP --include '*.ko' --exclude '*' out/modinstall/ $AK2DIR/modules/
+mkdir -p "$AK2DIR/modules/system/lib/modules/pronto"
+mv "$AK2DIR/modules/system/lib/modules/wlan.ko" "$AK2DIR/modules/system/lib/modules/pronto/pronto_wlan.ko"
+
+cp  $KDIR/out/arch/arm/boot/zImage $AK2DIR
+cp  $KDIR/out/arch/arm/boot/dts/qcom/*.dtb $AK2DIR
+
+cd $AK2DIR
+
+if [ -e $AK2DIR/*.zip ];
+then
+	rm *.zip
+fi;
+
+zip -r9 $FINAL_ZIP * -x .git README.md *placeholder > /dev/null
+
+if [ -e $FINAL_ZIP ];
+then
+	echo "==> Flashable zip created"
+	echo "==> Flashable zip is stored in $AK2DIR folder with name $FINAL_ZIP"
+	exit 0
+else
+	echo "!!! Failed to make zip. Abort !!!"
+	exit 1
+fi;
---
+++ scripts/zip.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+if [ -z $1 ] || [ -z $2 ]
+then
+	printf "\nUsage: \n\n\tbash zip.sh version release_type\n\n"
+	exit 1
+fi
+
+KDIR=$PWD
+AK2DIR=~/AnyKernel2
+DATE=$(date +"%m%d%y")
+KNAME="OrgasmKernel"
+export DEVICE="perry"
+export KBUILD_BUILD_USER="RblLn"
+export KBUILD_BUILD_HOST="PleasureBox"
+VER="-v$1"
+TYPE="_$2"
+export FINAL_ZIP="$KNAME"-"$DEVICE""$TYPE""$VER"_"$DATE".zip
+
+echo "==> Making Flashable zip"
+
+echo "=> Finding modules"
+
+rsync -aP --include '*.ko' --exclude '*' out/modinstall/ $AK2DIR/modules/
+mkdir -p "$AK2DIR/modules/system/lib/modules/pronto"
+mv "$AK2DIR/modules/system/lib/modules/wlan.ko" "$AK2DIR/modules/system/lib/modules/pronto/pronto_wlan.ko"
+
+cp  $KDIR/out/arch/arm/boot/zImage $AK2DIR
+cp  $KDIR/out/arch/arm/boot/dts/qcom/*.dtb $AK2DIR
+
+cd $AK2DIR
+
+if [ -e $AK2DIR/*.zip ];
+then
+	rm *.zip
+fi;
+
+zip -r9 $FINAL_ZIP * -x .git README.md *placeholder > /dev/null
+
+if [ -e $FINAL_ZIP ];
+then
+	echo "==> Flashable zip Created"
+	echo "==> Flashable zip is stored in $AK2DIR folder with name $FINAL_ZIP"
+	exit 0
+else
+	echo "!!! Failed to make zip. Abort !!!"
+	exit 1
+fi;
+
