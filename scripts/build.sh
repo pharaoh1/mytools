aaf362918fbf26b533665da4988b44f0a6d4eb8e
RebelLion420 <gaigecarlos@gmail.com>
2018-09-14 11:48:47 -0700

    scripts: Reworked gdrive upload of failure log for build.sh Cleaned up overall code
    Signed-off-by: RebelLion420 <gaigecarlos@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-if [ -z $2 ] || [ -z $3 ]
-then
+if [ -z $2 ] || [ -z $3 ] ; then
 	printf "\nUsage: \n\n\tbash build.sh [thread_amount] version_# release_type\n\n\tNOTE: '[thread_amount]' can be an integer or 'auto'.\n\n"
 	exit 1
 fi
@@ -24,8 +23,7 @@ TYPE="_$3"
 export FINAL_ZIP="$KNAME"-"$DEVICE""$TYPE""$VER"_"$DATE".zip
 
 # Sanity check to avoid using erroneous binaries
-if [ -e  out/arch/$ARCH/boot/*Image* ]
-	then
+if [ -e  out/arch/$ARCH/boot/*Image* ] ; then
 	rm -r out
 	rm $AK2DIR/*.dtb
 	rm $AK2DIR/*Image*
@@ -37,8 +35,7 @@ if [ -e  out/arch/$ARCH/boot/*Image* ]
 	touch $AK2DIR/modules/system/lib/modules/placeholder
 fi
 
-if [ "$1" == 'auto' ]
-then
+if [ "$1" == 'auto' ] ; then
 	t=$(nproc --all)
 else
 	t=$1
@@ -48,31 +45,31 @@ printf "\nTHREADS: $t\nVERSION: $2\nRELEASE: $3\nGCC VERSION: $GCCV\n\n"
 echo "==> Adapted build script, courtest of @facuarmo"
 echo "==> Making kernel binary..."
 make O=out perry_defconfig
-make O=out -j$t zImage |& tee build.log
-if [ $? -ne 0 ]
-then
+make O=out -j$t zImage |& tee fail.log
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then
 	echo "!!! Kernel compilation failed, can't continue !!!"
-	exit 1
+	gdrive upload --delete fail.log
+	exit 2
 fi
 echo "=> Making modules..."
-make O=out -j$t modules
-if [ $? -ne 0 ]
-then
+make O=out -j$t modules |& tee -a fail.log
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then
 	echo "Module compilation failed, can't continue."
+	gdrive upload --delete fail.log
 	exit 1
 fi
 rm -rf out/modinstall
 mkdir out/modinstall
-make O=out -j$t modules_install INSTALL_MOD_PATH=modinstall INSTALL_MOD_STRIP=1
-if [ $? -ne 0 ]
-then
+make O=out -j$t modules_install INSTALL_MOD_PATH=modinstall INSTALL_MOD_STRIP=1 |& tee -a fail.log
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then
 	echo "Module installation failed, can't continue."
+	gdrive upload --delete fail.log
 	exit 1
 fi
 
 # One more sanity check
-if [ -e $AK2DIR/*Image* ]
-then
+if [ -e $AK2DIR/*Image* ] ; then
+	rm $AK2DIR/*.zip
 	rm $AK2DIR/*Image*
 	rm $AK2DIR/*.dtb
 	rm $AK2DIR/modules/system/lib/modules/*.ko
@@ -93,15 +90,9 @@ cp  $KDIR/out/arch/$ARCH/boot/zImage $AK2DIR
 
 cd $AK2DIR
 
-if [ -e $AK2DIR/*.zip ]
-then
-	rm *.zip
-fi
-
 zip -r9 $FINAL_ZIP * -x .git README.md *placeholder > /dev/null
 
-if [ -e $FINAL_ZIP ]
-then
+if [ -e $FINAL_ZIP ] ; then
 	echo "==> Flashable zip created"
 	echo "==> Uploading $FINAL_ZIP to Google Drive"
 	gdrive upload --delete $AK2DIR/$FINAL_ZIP
