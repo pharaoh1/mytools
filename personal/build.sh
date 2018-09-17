3921bd471c5db6e0290b97e26a2e9b314105729b
RebelLion420 <gaigecarlos@gmail.com>
2018-09-16 22:26:54 -0700

    personal: My own edits apart from facuarmo's help
    Signed-off-by: RebelLion420 <gaigecarlos@gmail.com>

--- personal/build.sh
+++ personal/build.sh
@@ -1,17 +1,19 @@
-if [ -z $2 ] || [ -z $3 ]
-then
-	printf "\nUsage: \n\n\tbash build.sh [thread_amount] version_# release_type\n\n\tNOTE: '[thread_amount]' can be an integer or 'auto'.\n\n"
+#!/usr/bin/env bash
+if [ -z $2 ] || [ -z $3 ] ; then
+	printf "\nUsage: \n\n\tbash build.sh <thread_amount> <version_#> <release_type> <make_clean>\n\n\tNOTE: '<thread_amount>' can be an integer or 'auto'.\n\n\t'<make_clean>' is either 'ya' (yes after), 'yb' (yes before), 'b' (both) or blank\n\n"
 	exit 1
 fi
 
-KERNEL_DIR=$PWD
-AK2DIR=$KERNEL_DIR/AnyKernel2
-TOOLCHAINDIR=/home/linux/mytools/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+KDIR=$PWD
+AK2DIR=~/AnyKernel2
+TCDIR=~/mytools/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 DATE=$(date +"%m%d%y")
-KERNEL_NAME="OrgasmKernel"
+KNAME="OrgasmKernel"
+IMG=zImage
 
-export ARCH=arm && export SUBARCH=arm
-export CROSS_COMPILE=$TOOLCHAINDIR
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=$TCDIR
 export USE_CCACHE=1
 export COMPRESS_CACHE=1
 export DEVICE="perry"
@@ -19,47 +21,65 @@ export KBUILD_BUILD_USER="RblLn"
 export KBUILD_BUILD_HOST="PleasureBox"
 VER="-v$2"
 TYPE="_$3"
-export FINAL_ZIP="$KERNEL_NAME"-"$DEVICE""$TYPE""$VER"_"$DATE".zip
+export FINAL_ZIP="$KNAME"-"$DEVICE""$TYPE""$VER"_"$DATE".zip
 
-if [ -e  out/arch/arm/boot/zImage ];
-	then
-	rm -r out #Just to make sure it doesn't make flashable zip with previous zImage
-	rm -r $AK2DIR/modules/*
-	make clean
-	make mrproper
-	mkdir out
-fi;
+# Sanity check to avoid using erroneous binaries
+if [ -e  out/arch/$ARCH/boot/$IMG ]; then
+#	rm $AK2DIR/*.dtb
+	rm $AK2DIR/$IMG
+	rm -rf $AK2DIR/modules/system/lib/modules/*
+	touch $AK2DIR/modules/system/lib/modules/placeholder
+fi
 
-if [ "$1" == 'auto' ]
-then
+if [ "$1" == 'auto' ]; then
 	t=$(nproc --all)
 else
 	t=$1
 fi
+if [ "$4" == 'yb' ] || [ "$4" == 'b']; then
+	echo "Hold on a sec..."
+	make clean
+	make mrproper
+fi
+
 GCCV=$("$CROSS_COMPILE"gcc -v 2>&1 | tail -1 | cut -d ' ' -f 3)
 printf "\nTHREADS: $t\nVERSION: $2\nRELEASE: $3\nGCC VERSION: $GCCV\n\n"
 echo "==> Adapted build script, courtest of @facuarmo"
 echo "==> Making kernel binary..."
 make O=out perry_defconfig
-make O=out -j$t
-if [ $? -ne 0 ]
-then
+make O=out -j$t $IMG |& tee fail.log
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
 	echo "!!! Kernel compilation failed, can't continue !!!"
-	exit 1
+	gdrive upload --delete fail.log
+	exit 2
 fi
 echo "=> Making modules..."
-make O=out -j$t M=$PWD/vendor/qcom/opensource/wlan/prima ARCH=arm CROSS_COMPILE=$TOOLCHAINDIR modules WLAN_ROOT=$PWD/vendor/qcom/opensource/wlan/prima MODNAME=wlan BOARD_PLATFORM=msm8937 CONFIG_PRONTO_WLAN=m
-if [ $? -ne 0 ]
-then
+make O=out -j$t modules |& tee -a fail.log
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
 	echo "Module compilation failed, can't continue."
+	gdrive upload --delete fail.log
 	exit 1
 fi
-#make -j$t modules_install INSTALL_MOD_PATH=out/modules INSTALL_MOD_STRIP=1
-#if [ $? -ne 0 ]
-#then
-#	echo "Module installation failed, can't continue."
-#	exit 1
-#fi
+rm -rf out/modinstall
+mkdir out/modinstall
+make O=out -j$t modules_install INSTALL_MOD_PATH=modinstall INSTALL_MOD_STRIP=1 |& tee -a fail.log
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+	echo "Module installation failed, can't continue."
+	gdrive upload --delete fail.log
+	exit 1
+fi
+
+if [ -e fail.log ]; then
+	rm fail.log
+fi
+
+# One more sanity check
+if [ -e $AK2DIR/$IMG ]; then
+	rm $AK2DIR/$IMG
+#	rm $AK2DIR/*.dtb
+	rm -rf $AK2DIR/modules/system/lib/modules/*
+	touch $AK2DIR/modules/system/lib/modules/placeholder
+fi
 
 echo "==> Kernel compilation completed"
 
@@ -67,28 +87,31 @@ echo "==> Making Flashable zip"
 
 echo "=> Finding modules"
 
-rsync -a --prune-empty-dirs --include '*/' --include '*.ko' --exclude '*' out/ $AK2DIR/modules/
-mkdir -p "$AK2DIR/modules/system/lib/modules/pronto"
-cp "$AK2DIR/modules/system/lib/modules/wlan.ko" "$AK2DIR/modules/system/lib/modules/pronto/pronto_wlan.ko"
+find out/modinstall/ -name '*.ko' -type f -exec cp '{}' "$AK2DIR/modules/system/lib/modules/" \;
+mkdir -p "$AK2DIR/modules/system/lib/modules/prima"
+mv "$AK2DIR/modules/system/lib/modules/wlan.ko" "$AK2DIR/modules/system/lib/modules/prima/wlan.ko"
 
-cp  $KERNEL_DIR/out/arch/arm/boot/zImage $AK2DIR
-cp  $KERNEL_DIR/out/arch/arm/boot/dts/qcom/*.dtb $AK2DIR
+cp  $KDIR/out/arch/$ARCH/boot/$IMG $AK2DIR
+#cp  $KDIR/out/arch/$ARCH/boot/dts/qcom/*.dtb $AK2DIR
 
 cd $AK2DIR
 
-if [ -e $AK2DIR/*.zip ];
-then
-rm *.zip
-fi;
+zip -r9 $FINAL_ZIP * -x .git README.md *placeholder > /dev/null
 
-zip -r9 $FINAL_ZIP * -x *.zip $FINAL_ZIP > /dev/null
-
-if [ -e $FINAL_ZIP ];
-then
+if [ -e $FINAL_ZIP ]; then
 	echo "==> Flashable zip created"
-	echo "==> Flashable zip is stored in $AK2DIR folder with name $FINAL_ZIP"
+	echo "==> Uploading $FINAL_ZIP to Google Drive"
+	gdrive upload --delete $AK2DIR/$FINAL_ZIP
+	echo "==> Upload complete!"
+	echo "*** Enjoy your kernel! ***"
+	if [ "$4" == 'ya' ] || [ "$4" == 'b']; then
+		echo "Cleaning up..."
+		make clean
+		make mrproper
+	fi
 	exit 0
 else
-	echo "!!! Failed to make zip. Abort !!!"
+	echo "!!! Unexpected error. Abort !!!"
 	exit 1
-fi;
+fi
+ 
