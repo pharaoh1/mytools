a55e4e9b4f37fa9b7296b7262db0ab00eb69a589
Facundo Montero <facumo.fm@gmail.com>
2018-09-19 08:13:15 -0300

    Functions instead of running 75% equal code blocks
    Also use OR comparators instead of multiple if blocks to improve performance.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,61 +1,81 @@
 #!/usr/bin/env bash
+# Uncomment lines starting with '###' if your build produces Device Tree Blob (.dtb) files
 if [ -z $2 ] || [ -z $3 ] ; then
-	printf "\nUsage: \n\n\tbash build.sh [thread_amount] version_# release_type\n\n\tNOTE: '[thread_amount]' can be an integer or 'auto'.\n\n"
+	printf "\nUsage: \n\n\tbash build.sh <thread_amount> <version_#> <release_type> <make_clean>\n\n\tNOTE: '<thread_amount>' can be an integer or 'auto'.\n\n\t'<make_clean>' is either 'ya' (yes after), 'yb' (yes before), 'b' (both) or blank\n\n"
 	exit 1
 fi
 
-KDIR=$PWD
-AK2DIR=~/AnyKernel2
-TCDIR=~/mytools/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-DATE=$(date +"%m%d%y")
-KNAME="OrgasmKernel"
+# Function definition
 
+function fail()
+{
+	echo $1
+        gdrive upload --delete fail.log
+        exit 1
+}
+
+# Adjust these variables for your build
+KNAME="OrgasmKernel"
+IMG=zImage
+TCDIR=~/mytools/toolchains/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+AK2DIR=~/AnyKernel2
 export ARCH=arm
-export SUBARCH=arm
-export CROSS_COMPILE=$TCDIR
-export USE_CCACHE=1
-export COMPRESS_CACHE=1
 export DEVICE="perry"
 export KBUILD_BUILD_USER="RblLn"
 export KBUILD_BUILD_HOST="PleasureBox"
+#
+
+export SUBARCH=$ARCH
+export CROSS_COMPILE=$TCDIR
+export USE_CCACHE=1
+export COMPRESS_CACHE=1
+KDIR=$PWD
+DATE=$(date +"%m%d%y")
 VER="-v$2"
 TYPE="_$3"
-export FINAL_ZIP="$KNAME"-"$DEVICE""$TYPE""$VER"_"$DATE".zip
+FINAL_ZIP="$KNAME"-"$DEVICE""$TYPE""$VER"_"$DATE".zip
+GCCV=$("$CROSS_COMPILE"gcc -v 2>&1 | tail -1 | cut -d ' ' -f 3)
 
 # Sanity check to avoid using erroneous binaries
-if [ -e  out/arch/$ARCH/boot/*Image* ] ; then
-	rm -r out
-#	rm $AK2DIR/*.dtb
-	rm $AK2DIR/*Image*
-	rm -r $AK2DIR/modules/*
-	make clean
-	make mrproper
-	mkdir out
-	mkdir -p $AK2DIR/modules/system/lib/modules
-	touch $AK2DIR/modules/system/lib/modules/placeholder
+if [ -e  out/arch/$ARCH/boot/$IMG ]; then
+	rm -rf out/
+	mkdir -p out/modinstall
 fi
 
-if [ "$1" == 'auto' ] ; then
+if [ $1 == 'auto' ]; then
 	t=$(nproc --all)
 else
 	t=$1
 fi
-GCCV=$("$CROSS_COMPILE"gcc -v 2>&1 | tail -1 | cut -d ' ' -f 3)
+
+# Check if cleaning
+if [[ $4 == 'yb' ]] || [[ $4 == 'b' ]]; then
+	echo "==> Hold on a sec..."
+	make clean
+	make mrproper
+	echo "==> Ready!"
+fi
+
 printf "\nTHREADS: $t\nVERSION: $2\nRELEASE: $3\nGCC VERSION: $GCCV\n\n"
 echo "==> Adapted build script, courtest of @facuarmo"
+sleep 1
 echo "==> Making kernel binary..."
 make O=out perry_defconfig
-make O=out -j$t zImage 2> fail.log || echo "!!! Kernel compilation failed, can't continue !!!" && gdrive upload --delete fail.log && exit 2
+make O=out -j$t $IMG 2> fail.log || fail "!!! Kernel copilation failed, can't continue !!!"
 echo "=> Making modules..."
-make O=out -j$t modules 2>> fail.log || echo "Module compilation failed, can't continue." && gdrive upload --delete fail.log && exit 1
-rm -rf out/modinstall
-mkdir out/modinstall
-make O=out -j$t modules_install INSTALL_MOD_PATH=modinstall INSTALL_MOD_STRIP=1 2>> fail.log || echo "Module installation failed, can't continue." ; gdrive upload --delete fail.log ; exit 1
+make O=out -j$t modules 2>> fail.log || fail "Module compilation failed, can't continue."
+make O=out -j$t modules_install INSTALL_MOD_PATH=modinstall INSTALL_MOD_STRIP=1 2>> fail.log || fail "Module installation failed, can't continue."
+
+if [ -e fail.log ]; then
+	rm fail.log
+fi
+
 # One more sanity check
-if [ -e $AK2DIR/*Image* ] ; then
-	rm $AK2DIR/*Image*
-#	rm $AK2DIR/*.dtb
-	rm $AK2DIR/modules/system/lib/modules/pronto/*.ko
+if [ -e $AK2DIR/$IMG ]; then
+	rm $AK2DIR/$IMG
+###	rm $AK2DIR/*.dtb
+	rm -rf $AK2DIR/modules/system/lib/modules/*
+	touch $AK2DIR/modules/system/lib/modules/placeholder
 fi
 
 echo "==> Kernel compilation completed"
@@ -68,22 +88,30 @@ find out/modinstall/ -name '*.ko' -type f -exec cp '{}' "$AK2DIR/modules/system/
 mkdir -p "$AK2DIR/modules/system/lib/modules/pronto"
 mv "$AK2DIR/modules/system/lib/modules/wlan.ko" "$AK2DIR/modules/system/lib/modules/pronto/pronto_wlan.ko"
 
-cp  $KDIR/out/arch/$ARCH/boot/zImage $AK2DIR
-#cp  $KDIR/out/arch/$ARCH/boot/dts/qcom/*.dtb $AK2DIR
+cp  $KDIR/out/arch/$ARCH/boot/$IMG $AK2DIR
+###cp  $KDIR/out/arch/$ARCH/boot/dts/qcom/*.dtb $AK2DIR
 
 cd $AK2DIR
 
 zip -r9 $FINAL_ZIP * -x .git README.md *placeholder > /dev/null
 
-if [ -e $FINAL_ZIP ] ; then
+if [ -e $FINAL_ZIP ]; then
 	echo "==> Flashable zip created"
 	echo "==> Uploading $FINAL_ZIP to Google Drive"
 	gdrive upload --delete $AK2DIR/$FINAL_ZIP
 	echo "==> Upload complete!"
 	echo "*** Enjoy your kernel! ***"
+	if [[ $4 == 'ya' ]] || [[ $4 == 'b' ]]; then
+		cd $KDIR
+		echo "==> Cleaning up..."
+		make clean
+		make mrproper
+		rm -rf out/
+		mkdir -p out/modinstall
+		echo "==> All done!"
+	fi
 	exit 0
 else
 	echo "!!! Unexpected error. Abort !!!"
 	exit 1
 fi
-
